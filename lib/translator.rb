b6bf8b8f74f462cbc68fff6252c21e37c3f07da9
Paul Leugemors <pleugemors@yahoo.com>
2019-10-25 06:45:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,9 +14,10 @@ def load_library(path_name)
   ret_hash
 end
 
-def get_japanese_emoticon(w_emoticon)
+def get_japanese_emoticon(file_path, w_emoticon)
   # code goes here
-  
+  emot = load_library(file_path)
+  emot['get_emoticon'][w_emoticon]
 end
 
 def get_english_meaning
